488adb071e61b4772cdb45c23e10fd7afc3753f6
blam <ben@blam.sh>
2017-08-08 15:03:46 +0200

    Updating all the things

--- src/react-native.js
+++ src/react-native.js
@@ -106,7 +106,6 @@ const ReactNative = {
 // See http://facebook.github.io/react/docs/addons.html
 const ReactNativeAddons = {
   // LinkedStateMixin: require('react-addons-linked-state-mixin') deprecated,
-  Perf: require('react-addons-perf'),
   TestModule: require('./NativeModules/TestModule'),
   TestUtils: require('react-addons-test-utils'),
   // TODO(lmr): not sure where to find this
